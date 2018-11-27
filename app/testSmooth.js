33a030fa994331cd93e5026c20b3dcabaf11b873
Martin Leopold <m@martinleopold.com>
2018-11-27 21:08:16 +0100

    Added info about smooth() bug report

--- app/testSmooth.js
+++ app/testSmooth.js
@@ -1,6 +1,14 @@
+// Bug report posted Nov 27, 2018
+// https://github.com/processing/p5.js/issues/3348
+
 // Testing bug with smooth() in WEBGL
 // Once smooth() is called, switching projection (ortho or perspective) doesn't work anymore
 // Also smooth() seems to overthrow stroke() and fill() settings
+//
+// Instructions: 
+// * Run code as is. Rotating code is rendered with parallel projection.
+// * Click on cavas.  Cube should be rendered with perspective projection but isn't. 
+// * Set useSmooth to false and repeat steps to see desired behaviour.
 
 let useSmooth = true;
 let useOrtho = true;
@@ -11,7 +19,7 @@ function setup() {
   
   stroke(255);
   fill(0);
-  if (useSmooth) smooth(); // erases stroke and fill from before, 
+  if (useSmooth) smooth(); // erases stroke and fill from before
 }
 
 function draw() {
